36bd0d7e69cc00d31db2bd0dfca52016539f7daf
Daniel Fears <danieljfears@gmail.com>
2015-03-31 17:00:15 +0100

    GIFs working, and with multiple GIFs

--- app/src/main/java/danielfears/pokedex/CharmanderActivity.java
+++ app/src/main/java/danielfears/pokedex/CharmanderActivity.java
@@ -1,26 +1,25 @@
 package danielfears.pokedex;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceView;
+
 import gif.decoder.GifRun;
 
 
-public class KantoActivity extends ActionBarActivity {
+public class CharmanderActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kanto);
+        setContentView(R.layout.activity_charmander);
+
         SurfaceView v = (SurfaceView) findViewById(R.id.charmander);
-        v.setZOrderOnTop(true);    // necessary
-
+        v.setZOrderOnTop(true);
         GifRun w = new  GifRun();
-
         w.LoadGiff(v, this, R.drawable.charmander);
-
-
-
     }
 
 }
