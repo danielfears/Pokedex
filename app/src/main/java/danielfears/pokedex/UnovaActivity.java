01bae9e1506522146228f40dd24c1d0d32306d22
danieljfears <fea05095797@student.citybathcoll.ac.uk>
2015-04-01 14:24:10 +0100

    Unova + Pokemon activities created and linked

--- app/src/main/java/danielfears/pokedex/UnovaActivity.java
+++ app/src/main/java/danielfears/pokedex/UnovaActivity.java
@@ -1,9 +1,12 @@
 package danielfears.pokedex;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class UnovaActivity extends ActionBarActivity {
@@ -12,6 +15,36 @@ public class UnovaActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unova);
+
+        Button tepig = (Button) findViewById(R.id.tepig);
+
+        tepig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( UnovaActivity.this, TepigActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button snivy = (Button) findViewById(R.id.snivy);
+
+        snivy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( UnovaActivity.this, SnivyActivity.class);
+                startActivity(i);
+            }
+        });
+
+        Button oshawott = (Button) findViewById(R.id.oshawott);
+
+        oshawott.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent( UnovaActivity.this, OshawottActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
